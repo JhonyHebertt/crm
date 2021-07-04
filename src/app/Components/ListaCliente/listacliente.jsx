91327261bc47d7aaf01f7ae771b809311040b2ec
JhonyHebertt <51969572+JhonyHebertt@users.noreply.github.com>
2021-07-03 20:51:10 -0400

    Lista Clientes / Nome Clientes

--- src/app/Components/ListaCliente/listacliente.jsx
+++ src/app/Components/ListaCliente/listacliente.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './listacliente.css';
-import clientes from '../../Dados/clientes';
 
-function ListaClientes() {
+function ListaClientes(props) {
+
+    function deleteUser(id) {
+        alert('Exluir usuário ' + id);
+    }
+
     return <table className="table table-hover table-bordered">
         <thead>
             <tr className="table-secondary">
@@ -10,22 +15,29 @@ function ListaClientes() {
                 <th scope="col">Nome</th>
                 <th scope="col">E-mail</th>
                 <th scope="col">Telefone</th>
+                <th scope="col" className="col-acao"></th>
             </tr>
         </thead>
         <tbody>
+
             {
-                clientes.map((cliente) => {
+                props.arrayClientes.map((cliente) => {
                     return <tr key={cliente.id}>
                         <th scope="row">{cliente.id}</th>
-                        <td>{cliente.name}</td>
+                        <td>{cliente.nome}</td>
                         <td>{cliente.email}</td>
                         <td>{cliente.fone}</td>
+                        <td>
+                            <Link to='#'><i className="fas fa-edit icone-acao"></i></Link>
+                            <Link to='#' onClick={() => deleteUser(cliente.id)}><i className="far fa-trash-alt icone-acao red"></i></Link>
+                        </td>
                     </tr>
                 })
             }
 
-        </tbody>
-    </table>
+
+        </tbody >
+    </table >
 }
 
 export default ListaClientes;
--- src/app/Home/home.jsx
+++ src/app/Home/home.jsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../Components/Navbar/navbar';
 import ListaClientes from '../Components/ListaCliente/listacliente';
 import './home.css';
 
+import firebase from '../Config/firebase';
+
 function Home() {
+
+  const [clientes, setClientes] = useState([]);
+  const [texto, setTexto] = useState('');
+  const [busca, setBusca] = useState('');
+
+  let listaCli = [];
+
+  //useEffect(function () { Função }, [ quando chamar ]); por padrao [] vem em branco ou seja, vai excutar só quando abri a pag.
+  useEffect(function () {
+    firebase.firestore().collection('clientes').get().then(async function (resultado) {
+      await resultado.docs.forEach(function (doc) { //await força a useEffect esperar o termino do forEach
+        if (doc.data().nome.indexOf(busca) >= 0) {
+          listaCli.push({
+            id: doc.id,
+            nome: doc.data().nome,
+            email: doc.data().email,
+            fone: doc.data().fone
+          });
+        }
+      })
+
+      setClientes(listaCli);
+    })
+  }, [busca]);
+
   return <div>
     <Navbar />
     <div className="container-fluid titulo">
       <h1>Cadastro de Clientes</h1>
-      <ListaClientes />
+
+      <div className="row">
+        <div className="col-4">
+          <Link to='/app/novocliente' className="btn btn-primary" type="button"><i className="fas fa-plus"></i> Cliente</Link>
+        </div>
+
+        <div className="col-8">
+          <div className="input-group mb-3">
+            <input onChange={(t) => setTexto(t.target.value)} type="text" className="form-control" placeholder="Pesquisar por nome" aria-describedby="button-addon2" />
+            <button onClick={(e) => setBusca(texto)} className="btn btn-primary" type="button" id="button-addon2"><i className="fas fa-search"></i> Pesquisar</button>
+          </div>
+        </div>
+      </div>
+
+      <ListaClientes arrayClientes={clientes} />
     </div>
   </div>
 }
